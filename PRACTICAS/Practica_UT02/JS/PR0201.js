3dd2b6c474695f719a6aa8d4b45ca522fb05b18a
ernestoorod <ernessttooo16@gmail.com>
2023-09-27 19:59:39 +0200

    Primer Commit

---
+++ PRACTICAS/Practica_UT02/JS/PR0201.js
@@ -0,0 +1,7 @@
+var nombre = prompt("Por favor, ingresa tu nombre:");
+
+var localidad = prompt("Ahora, ingresa tu localidad:");
+
+var mensaje = "Hola " + nombre + ", eres de " + localidad + ".";
+
+console.log(mensaje);
