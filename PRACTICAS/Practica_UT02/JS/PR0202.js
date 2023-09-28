be843eef4c92ed7cc548767c773edfee28e7924c
ernestoorod <ernessttooo16@gmail.com>
2023-09-28 09:17:24 +0200

    2Commit

--- PRACTICAS/Practica_UT02/JS/PR0202.js
+++ PRACTICAS/Practica_UT02/JS/PR0202.js
@@ -1,7 +1,9 @@
 let nombre = prompt("Por favor, ingresa tu nombre:");
-let notaExamen1 = parseFloat(prompt("Ingresa la nota del primer examen:"));
-let notaExamen2 = parseFloat(prompt("Ingresa la nota del segundo examen:"));
-let notaMedia = (notaExamen1 + notaExamen2) / 2;
-let mensaje = `Hola ${nombre}, la nota media de tus exámenes es ${notaMedia.toFixed(2)}.`;
+let notaExamen1 = prompt("Ingresa la nota del primer examen:");
+let notaExamen2 = prompt("Ingresa la nota del segundo examen:");
+let notaExamen1_1 = parseFloat(notaExamen1);
+let notaExamen1_2 = parseFloat(notaExamen2);
+let notaMedia = (notaExamen1_1 + notaExamen1_2) / 2;
+let mensaje = `Hola ${nombre}, la nota media de tus exámenes es ${notaMedia}.`;
 
 console.log(mensaje);
