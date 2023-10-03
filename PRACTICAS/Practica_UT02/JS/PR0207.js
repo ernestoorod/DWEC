5ebfcedaa7833b328b68e1c9a348fa531856039a
ernestoorod <ernessttooo16@gmail.com>
2023-10-03 19:41:28 +0200

    commit

--- PRACTICAS/Practica_UT02/JS/PR0207.js
+++ PRACTICAS/Practica_UT02/JS/PR0207.js
@@ -1,22 +1,9 @@
-const readline = require('readline');
+let numero = prompt("Introduceme un numero");
+let k = "";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Por favor, ingrese un número entero: ', (input) => {
-  const num = parseInt(input);
-  
-  if (!isNaN(num) && num > 0) {
-    for (let i = 1; i <= num; i++) {
-      console.log('*'.repeat(i));
-    }
-  } else {
-    console.log('Por favor, ingrese un número entero positivo.');
-  }
-
-  rl.close();
-});
+for(let i = 1; i <= numero; i++){
+    k = `${k}*`;
+    console.log(k);
+}
 
 
